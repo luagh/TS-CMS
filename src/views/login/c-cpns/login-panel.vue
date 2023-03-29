faca92135c182029269368fd627ca9e66b7db4e4
laugh <laugh@email.com>
2023-03-29 22:15:41 +0800

    登录页面底部的搭建

---
+++ src/views/login/c-cpns/login-panel.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login-panel">
+    <h1 class="title">后台管理系统</h1>
+    <div class="tabs">
+      <el-tabs class="demo-tabs" type="border-card" stretch>
+        <el-tab-pane label="账号登录" name="first">User</el-tab-pane>
+        <el-tab-pane label="手机登录" name="second">Config</el-tab-pane>
+      </el-tabs>
+    </div>
+    <div class="controls">
+      <el-checkbox v-model="isRemPwd" label="记住密码" size="large" />
+      <el-link type="primary">忘记密码</el-link>
+    </div>
+    <el-button class="login-btn" type="primary" size="large"> 立即登录 </el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const isRemPwd = ref(false)
+</script>
+
+<style lang="less" scoped>
+.login-panel {
+  width: 330px;
+  margin-bottom: 150px;
+
+  .title {
+    text-align: center;
+    margin-bottom: 15px;
+  }
+
+  .label {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .text {
+      margin-left: 5px;
+    }
+  }
+
+  .controls {
+    margin-top: 12px;
+    display: flex;
+
+    justify-content: space-between;
+  }
+
+  .login-btn {
+    margin-top: 10px;
+    width: 100%;
+    // --el-button-size: 50px;
+  }
+}
+</style>
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="login">
-    <h2>login</h2>
+    <login-panel></login-panel>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoginPanel from './c-cpns/login-panel.vue'
+</script>
 
 <style lang="less" scoped>
 .login {
-  color: red;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: url('../../assets/img/login-bg.svg');
 }
 </style>
