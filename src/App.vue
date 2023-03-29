35182ac338445cf464eb038ac808c4935198eb57
laugh <laugh@email.com>
2023-03-29 15:33:42 +0800

    路由的配置

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>home</h2>
+    <router-link to="/main">主要</router-link>
+    <router-link to="/login">登录</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style></style>
