265af22ae6808bb1c5413030befccc7511728e2e
laugh <laugh@email.com>
2023-03-31 11:17:56 +0800

    menu菜单展示

--- src/components/main-menu/main-menu.vue
+++ src/components/main-menu/main-menu.vue
@@ -6,39 +6,54 @@
     </div>
     <div class="menu">
       <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
 
+        text-color="#b7bdc3"
+        active-text-color="#fff"
+        background-color="#001529"
       >
-        <el-sub-menu index="1">
+        <!-- 遍历整个菜单 -->
+
+       <!-- .系统总览 -->
+        <el-sub-menu>
           <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
+            <el-icon><Monitor /></el-icon>
+            <span>系统总览</span>
           </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
         </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
+
+        <!-- 2.系统管理 -->
+        <el-sub-menu>
+          <template #title>
+            <el-icon><Setting /></el-icon>
+            <span>系统管理</span>
+          </template>
+          <el-menu-item>用户管理</el-menu-item>
+          <el-menu-item>部门管理</el-menu-item>
+          <el-menu-item>菜单管理</el-menu-item>
+          <el-menu-item>角色管理</el-menu-item>
+        </el-sub-menu>
+
+        <!-- 3.商品中心 -->
+        <el-sub-menu>
+          <template #title>
+            <el-icon><ShoppingBag /></el-icon>
+            <span>商品中心</span>
+          </template>
+          <el-menu-item>商品类别</el-menu-item>
+          <el-menu-item>商品信息</el-menu-item>
+        </el-sub-menu>
+
+        <!-- 4.随便聊聊 -->
+         <el-sub-menu>
+          <template #title>
+            <el-icon><Monitor /></el-icon>
+            <span>随便聊聊</span>
+          </template>
+          <el-menu-item>你的故事</el-menu-item>
+          <el-menu-item>故事列表</el-menu-item>
+        </el-sub-menu>
       </el-menu>
     </div>
   </div>
@@ -47,7 +62,50 @@
 <script setup lang="ts"></script>
 
 <style lang="less" scoped>
-.main-menu{
-  color: aliceblue;
+.main-menu {
+  height: 100%;
+  background-color: #001529;
+}
+
+.logo {
+  display: flex;
+  height: 28px;
+  padding: 12px 10px 8px 10px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  overflow: hidden;
+
+  .img {
+    height: 100%;
+    margin: 0 10px;
+  }
+
+  .title {
+    font-size: 16px;
+    font-weight: 700;
+    color: white;
+    white-space: nowrap;
+  }
+}
+
+.el-menu {
+  border-right: none;
+  user-select: none;
+}
+
+.el-sub-menu {
+  .el-menu-item {
+    padding-left: 50px !important;
+    background-color: #0c2135;
+  }
+
+  .el-menu-item:hover {
+    color: #fff;
+  }
+
+  .el-menu-item.is-active {
+    background-color: #0a60bd;
+  }
 }
 </style>
