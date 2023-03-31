8c12e9ffbbe2a182d6ec5ef5751f7e397dc99658
laugh <laugh@email.com>
2023-03-31 14:47:41 +0800

    header个人信息展示和退出登录

--- src/components/main-header/c-cpns/header-info.vue
+++ src/components/main-header/c-cpns/header-info.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="header-info">
+    <!-- 1.操作小图标 -->
     <div class="operation">
       <span>
         <el-icon><Message /></el-icon>
@@ -12,14 +13,105 @@
         <el-icon><Search /></el-icon>
       </span>
     </div>
+
+    <!-- 2.个人信息 -->
+    <div class="info">
+      <el-dropdown>
+        <span class="user-info">
+          <el-avatar
+            :size="30"
+            src="https://upload.jianshu.io/users/upload_avatars/1102036/c3628b478f06.jpeg"
+          />
+          <span class="name">coderwhy</span>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item @click="handleExitClick">
+              <el-icon><CircleClose /></el-icon>
+              <span>退出系统</span>
+            </el-dropdown-item>
+            <el-dropdown-item divided>
+              <el-icon><InfoFilled /></el-icon>
+              <span>个人信息</span>
+            </el-dropdown-item>
+            <el-dropdown-item>
+              <el-icon><Unlock /></el-icon>
+              <span>修改密码</span>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
+
+const router = useRouter()
+function handleExitClick() {
+  localCache.removeCache(LOGIN_TOKEN)
+  router.push('/login')
+}
 </script>
 
 <style lang="less" scoped>
-.header-info{
-  color: #000;
+.header-info {
+  display: flex;
+  align-items: center;
+}
+
+.operation {
+  display: inline-flex;
+  margin-right: 20px;
+
+  span {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 35px;
+
+    &:hover {
+      background: #f2f2f2;
+    }
+
+    i {
+      font-size: 20px;
+    }
+
+    .dot {
+      position: absolute;
+      top: 3px;
+      right: 3px;
+      z-index: 10;
+      width: 6px;
+      height: 6px;
+      background: red;
+      border-radius: 100%;
+    }
+  }
+}
+
+.info {
+  .user-info {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .name {
+      margin-left: 5px;
+    }
+  }
+}
+
+.info {
+  :global(.el-dropdown-menu__item) {//全局变量
+    line-height: 36px !important;
+    padding: 6px 22px;
+  }
 }
 </style>
--- src/components/main-header/main-header.vue
+++ src/components/main-header/main-header.vue
@@ -2,36 +2,53 @@
   <div class="main-header">
     <div class="menu-icon" @click="handleMenuIconClick">
       <el-icon size="28px">
-        <!-- //动态组件 -->
-        <component :is="isFold ?'Expand':'Fold'"></component>
+        <component :is="isFold ? 'Expand' : 'Fold'" />
       </el-icon>
     </div>
     <div class="content">
       <div class="breadcrumb">面包屑</div>
-     <div class="info">个人信息</div>
+      <header-info />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref} from 'vue'
+import { ref } from 'vue'
+import HeaderInfo from './c-cpns/header-info.vue'
 
 // 0.内部自定义事件
-const emit =defineEmits(['foldChange'])
+const emit = defineEmits(['foldChange'])
+
 // 1.记录状态
- const isFold =ref(false)
-function handleMenuIconClick(){
+const isFold = ref(false)
+function handleMenuIconClick() {
   // 1.内部改变状态
- isFold.value = !isFold.value
-//将事件和状态传递给父组件
-emit('foldChange',isFold.value)
+  isFold.value = !isFold.value
 
+  // 2.将事件和状态传递给父组件
+  emit('foldChange', isFold.value)
 }
-
 </script>
 
 <style lang="less" scoped>
-.main-header{
-  color: #000;
+.main-header {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+
+  .menu-icon {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 18px;
+  }
 }
 </style>
