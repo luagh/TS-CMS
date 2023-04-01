556cc99f9b360154fa75eaaf6ad3933634f21536
laugh <laugh@email.com>
2023-04-01 17:22:03 +0800

    content页面数据的展示和部分样式

---
+++ src/store/main/system/system.ts
@@ -0,0 +1,21 @@
+import { postUserListData } from '@/service/main/system/system'
+import { defineStore} from 'pinia'
+import type { ISystemState } from './type'
+
+
+const  useSystemStore =defineStore('system',{
+  state: ():ISystemState => ( {
+    usersList:[],
+    usersTotalCount:0
+  }),
+  actions: {
+  async   postUsersListAction (){
+  const usersListResult =await postUserListData()
+  const { totalCount,list} =usersListResult.data
+  this.usersTotalCount = totalCount
+  this.usersList = list
+ }
+  }
+})
+
+export default useSystemStore
