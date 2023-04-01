7be7351869f494ebe92ba2e402c68ea790f3ecc4
laugh <laugh@email.com>
2023-04-01 15:08:08 +0800

    search表单搜索框页面搭建

--- src/views/main/system/user/c-cpns/user-search.vue
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="user-search">
-    <h2>user-search</h2>
+    <!-- 1.输入搜索关键字的表单 -->
+    <el-form label-width="80px" size="large">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item label="用户名" prop="name">
+            <el-input
+              placeholder="请输入查询的用户名"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="真实姓名" prop="realname">
+            <el-input
+              placeholder="请输入查询的真实姓名"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="手机号码" prop="cellphone">
+            <el-input
+              placeholder="请输入查询的手机号码"
+            />
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="8">
+          <el-form-item label="状态" prop="enable">
+            <el-select
+              placeholder="请选择查询的状态"
+              style="width: 100%"
+            >
+              <el-option label="启用" :value="1" />
+              <el-option label="禁用" :value="0" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="创建时间" prop="createAt">
+            <el-date-picker
+              type="daterange"
+              range-separator="-"
+              start-placeholder="开始时间"
+              end-placeholder="结束时间"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
   </div>
 </template>
 
@@ -10,6 +57,10 @@
 .user-search{
 background-color: #fff;
 padding: 20px;
+.el-form-item {
+    padding: 20px 30px;
+    margin-bottom: 0;
+  }
 }
 
 </style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="user">
     <user-search></user-search>
+    <div class="content">
+      <div class="header">头部</div>
+      <div class="table">表格</div>
+      <div class="pagination">分页</div>
+    </div>
   </div>
 </template>
 
