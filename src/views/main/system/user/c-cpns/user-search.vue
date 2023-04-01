4052f48381e2873023732e5fd4daea876907cb0f
laugh <laugh@email.com>
2023-04-01 15:50:23 +0800

    search页面重置功能的实现

--- src/views/main/system/user/c-cpns/user-search.vue
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="user-search">
     <!-- 1.输入搜索关键字的表单 -->
-    <el-form label-width="80px" size="large">
+    <el-form label-width="80px" size="large" :model="searchForm">
       <el-row :gutter="20">
         <el-col :span="8">
           <el-form-item label="用户名" prop="name">
             <el-input
+            v-model="searchForm.name"
               placeholder="请输入查询的用户名"
             />
           </el-form-item>
@@ -13,6 +14,7 @@
         <el-col :span="8">
           <el-form-item label="真实姓名" prop="realname">
             <el-input
+          v-model="searchForm.realname"
               placeholder="请输入查询的真实姓名"
             />
           </el-form-item>
@@ -20,6 +22,7 @@
         <el-col :span="8">
           <el-form-item label="手机号码" prop="cellphone">
             <el-input
+            v-model="searchForm.cellphone"
               placeholder="请输入查询的手机号码"
             />
           </el-form-item>
@@ -27,6 +30,7 @@
         <el-col :span="8">
           <el-form-item label="状态" prop="enable">
             <el-select
+            v-model="searchForm.enable"
               placeholder="请选择查询的状态"
               style="width: 100%"
             >
@@ -38,6 +42,7 @@
         <el-col :span="8">
           <el-form-item label="创建时间" prop="createAt">
             <el-date-picker
+            v-model="searchForm.createAt"
               type="daterange"
               range-separator="-"
               start-placeholder="开始时间"
@@ -49,14 +54,37 @@
     </el-form>
      <!-- 2.重置和搜索的按钮 -->
      <div class="btns">
-      <el-button icon="Refresh" >重置</el-button>
-      <el-button icon="Search" type="primary">查询</el-button
+      <el-button icon="Refresh" @click="handleResetClick">重置</el-button>
+      <el-button icon="Search" type="primary" @click="handleQueryClick">查询</el-button
       >
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ElForm } from 'element-plus';
+import { reactive, ref } from 'vue';
+
+
+const searchForm =reactive({
+  name: '',
+  realname: '',
+  cellphone: '',
+  enable: 1,
+  createAt: []
+})
+
+// 重置
+const formRef =ref<InstanceType<typeof ElForm>>()
+function handleResetClick(){
+formRef.value?.resetFields()
+}
+// 搜索
+function handleQueryClick (){
+  console.log('handleQueryClick')
+}
+
+</script>
 
 <style lang="less" scoped>
 .user-search{
