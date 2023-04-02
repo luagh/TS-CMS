25611bb7c7de77eb490058017dc36ad9b2862ccd
laugh <laugh@email.com>
2023-04-02 18:33:39 +0800

    user页面新建按钮弹出框布局展示

--- src/views/main/system/user/c-cpns/user-modal.vue
+++ src/views/main/system/user/c-cpns/user-modal.vue
@@ -1,10 +1,54 @@
 <template>
   <div class="user-modal">
     <el-dialog v-model="dialogVisible" title="Warning" width="30%" center>
-    <span>
-      It should be noted that the content will not be aligned in center by
-      default
-    </span>
+      <div class="form">
+        <el-form>
+        <el-form-item label="用户名" prop="name">
+            <el-input v-model="formData.name" placeholder="请输入用户名" />
+          </el-form-item>
+          <el-form-item label="真实姓名" prop="realname">
+            <el-input
+              v-model="formData.realname"
+              placeholder="请输入真实姓名"
+            />
+          </el-form-item>
+          <el-form-item  label="密码" prop="password">
+            <el-input
+              v-model="formData.password"
+              placeholder="请输入密码"
+              show-password
+            />
+          </el-form-item>
+          <el-form-item label="手机号码" prop="cellphone">
+            <el-input
+              v-model="formData.cellphone"
+              placeholder="请输入手机号码"
+            />
+          </el-form-item>
+          <el-form-item label="选择角色" prop="roleId">
+            <el-select
+              v-model="formData.roleId"
+              placeholder="请选择角色"
+              style="width: 100%"
+            >
+
+            </el-select>
+          </el-form-item>
+          <el-form-item label="选择部门" prop="departmentId">
+            <el-select
+              v-model="formData.departmentId"
+              placeholder="请选择部门"
+              style="width: 100%"
+            >
+              <template >
+
+              </template>
+            </el-select>
+          </el-form-item>
+        </el-form>
+
+      </div>
+
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
@@ -18,13 +62,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 
 
 // 1.定义内部的属性
 const dialogVisible = ref(false)
-
+const formData= reactive<any>({
+     name: '',
+     realname: '',
+     password: '',
+     cellphone: '',
+     roleId: '',
+     departmentId: ''
+})
 
 function  setModalVisible(){
   dialogVisible.value=true
@@ -34,6 +85,7 @@ defineExpose({ setModalVisible })
 </script>
 
 <style lang="less" scoped>
-.user-modal{
+.form {
+  padding: 0 20px;
 }
 </style>
