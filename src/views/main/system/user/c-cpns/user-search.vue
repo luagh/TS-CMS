2fdd99c27f113f99d859956c2ebc8c15c87a7243
laugh <laugh@email.com>
2023-04-02 15:15:23 +0800

    点击查询和重置按钮功能

--- src/views/main/system/user/c-cpns/user-search.vue
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="user-search">
+  <div class="search">
     <!-- 1.输入搜索关键字的表单 -->
-    <el-form label-width="80px" size="large" :model="searchForm">
+    <el-form :model="searchForm" ref="formRef" label-width="80px" size="large">
       <el-row :gutter="20">
         <el-col :span="8">
           <el-form-item label="用户名" prop="name">
             <el-input
-            v-model="searchForm.name"
+              v-model="searchForm.name"
               placeholder="请输入查询的用户名"
             />
           </el-form-item>
@@ -14,7 +14,7 @@
         <el-col :span="8">
           <el-form-item label="真实姓名" prop="realname">
             <el-input
-          v-model="searchForm.realname"
+              v-model="searchForm.realname"
               placeholder="请输入查询的真实姓名"
             />
           </el-form-item>
@@ -22,15 +22,16 @@
         <el-col :span="8">
           <el-form-item label="手机号码" prop="cellphone">
             <el-input
-            v-model="searchForm.cellphone"
+              v-model="searchForm.cellphone"
               placeholder="请输入查询的手机号码"
             />
           </el-form-item>
         </el-col>
+
         <el-col :span="8">
           <el-form-item label="状态" prop="enable">
             <el-select
-            v-model="searchForm.enable"
+              v-model="searchForm.enable"
               placeholder="请选择查询的状态"
               style="width: 100%"
             >
@@ -42,7 +43,7 @@
         <el-col :span="8">
           <el-form-item label="创建时间" prop="createAt">
             <el-date-picker
-            v-model="searchForm.createAt"
+              v-model="searchForm.createAt"
               type="daterange"
               range-separator="-"
               start-placeholder="开始时间"
@@ -52,48 +53,58 @@
         </el-col>
       </el-row>
     </el-form>
-     <!-- 2.重置和搜索的按钮 -->
-     <div class="btns">
+
+    <!-- 2.重置和搜索的按钮 -->
+    <div class="btns">
       <el-button icon="Refresh" @click="handleResetClick">重置</el-button>
-      <el-button icon="Search" type="primary" @click="handleQueryClick">查询</el-button
+      <el-button icon="Search" type="primary" @click="handleQueryClick"
+        >查询</el-button
       >
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { ElForm } from 'element-plus';
-import { reactive, ref } from 'vue';
+import { reactive, ref } from 'vue'
+import type { ElForm } from 'element-plus'
 
+// 定义自定义事件
+const emit = defineEmits(['queryClick', 'resetClick'])
 
-const searchForm =reactive({
+// 定义form的数据
+const searchForm = reactive({
   name: '',
   realname: '',
   cellphone: '',
-  enable: 1,
-  createAt: []
+  enable: '',
+  createAt: ''
 })
 
-// 重置
-const formRef =ref<InstanceType<typeof ElForm>>()
-function handleResetClick(){
-formRef.value?.resetFields()
-}
-// 搜索
-function handleQueryClick (){
-  console.log('handleQueryClick')
+// 重置操作
+const formRef = ref<InstanceType<typeof ElForm>>()
+function handleResetClick() {
+  // 1.form中的数据全部重置
+  formRef.value?.resetFields()
+
+  // 2.将事件出去, content内部重新发送网络请求
+  emit('resetClick')
 }
 
+function handleQueryClick() {
+  emit('queryClick', searchForm)
+}
 </script>
 
 <style lang="less" scoped>
-.user-search{
-background-color: #fff;
-padding: 20px;
-.el-form-item {
+.search {
+  background-color: #fff;
+  padding: 20px;
+
+  .el-form-item {
     padding: 20px 30px;
     margin-bottom: 0;
   }
+
   .btns {
     text-align: right;
     padding: 0 50px 10px 0;
@@ -103,5 +114,4 @@ padding: 20px;
     }
   }
 }
-
 </style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="user">
-    <user-search></user-search>
-    <user-content></user-content>
+    <user-search
+      @query-click="handleQueryClick"
+      @reset-click="handleResetClick"
+    />
+    <user-content
+      ref="contentRef"
+
+    />
+    <user-modal ref="modalRef" />
   </div>
 </template>
 
 <script setup lang="ts">
-import userSearch from './c-cpns/user-search.vue';
-import userContent from './c-cpns/user-content.vue';
+import { ref } from 'vue'
+import UserSearch from './c-cpns/user-search.vue'
+import UserContent from './c-cpns/user-content.vue'
+
+
+// 对content组件的操作
+const contentRef = ref<InstanceType<typeof UserContent>>()
+function handleQueryClick(formData: any) {
+  contentRef.value?.fetchUserListData(formData)
+}
+function handleResetClick() {
+  contentRef.value?.fetchUserListData()
+}
+
+
 </script>
 
 <style lang="less" scoped>
 .user {
-  color: red;
-  border-radius: 9px;
+  border-radius: 8px;
   overflow: hidden;
 }
 </style>
