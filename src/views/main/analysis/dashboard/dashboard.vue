be3ffdb1a711013a7ad2db1ed278acdb8f04d6aa
laugh <laugh@email.com>
2023-04-05 17:31:32 +0800

    数据统计页面-顶部card组件的封装

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="dashboard">
-    <h2>dashboard</h2>
+   <el-row :gutter="10">
+ <template v-for="item in 4" :key="item">
+  <el-col :span="6">
+  <count-card></count-card>
+  </el-col>
+ </template>
+   </el-row>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CountCard from './c-cpns/count-card.vue'
+
+
+</script>
 
 <style lang="less" scoped>
 .dashboard {
   color: red;
+}
+
+.el-row {
+  margin-bottom: 10px;
 }
 </style>
